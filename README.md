f7f600323bb516315336368b3e2f2438f7e7e075
yuricorr <yuri_correa_oliveira@hotmail.com>
2025-07-25 11:14:46 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Calculadora Infusões
+
+Este é um projeto PWA.
---
+++ service-worker.js
@@ -0,0 +1,1 @@
+self.addEventListener('install', e => { e.waitUntil(caches.open('v1').then(cache => cache.addAll(['/index.html']))); });
