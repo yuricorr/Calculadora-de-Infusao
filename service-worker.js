bc84a31598ab17be229754b586edb73ef0133561
yuricorr <yuri_correa_oliveira@hotmail.com>
2025-07-29 23:15:12 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+
+const CACHE_NAME = "calculadora-uti-cache-v1";
+const urlsToCache = [
+  "index_abas_funcionais_fixas_topo.html",
+  "icon-192.png",
+  "icon-512.png",
+  "manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
